23a12d0450569ce62576c5a23bfaaf56be92a281
CLAREVOYANT <56900294+clare-lee@users.noreply.github.com>
2025-10-30 11:03:04 -0400

    feat: add styling with ANSI escape codes

--- scraper.py
+++ scraper.py
@@ -9,32 +9,35 @@ try:
 
     soup = BeautifulSoup(page.content, "html.parser")
 
+    # ANSI escape codes
+    BOLD = "\033[1m"
+    RESET = "\033[0m"
+
     # print location in single line
     # text = location.get_text().replace("Lon:", " Lon:").replace("Elev:", " Elev:")
     # print("Location:", text.strip())
 
     location = soup.find("span", class_="smallTxt")
+
     if location:
-        print("Location:")
+        print(f"{BOLD}Location:{RESET}")
         for index, item in enumerate(location, start=1):
             text = item.text.strip()
             if text:
-                print(text, end=" ")
+                print(f" {text}", end=" ")
                 if index % 2 == 0:
                     print()
+    print()
 
     # Get current conditions
     current_f = soup.find("p", class_="myforecast-current-lrg")
     current_c = soup.find("p", class_="myforecast-current-sm")
     current_condition = soup.find("p", class_="myforecast-current")
 
-    print(
-        "Current Weather:",
-        current_f.get_text(),
-        "/",
-        current_c.get_text(),
-        current_condition.get_text(),
-    )
+    print(f"{BOLD}Current Weather:{RESET}")
+    print(f"  Temperature: {current_f.get_text()} / {current_c.get_text()}")
+    print(f"  Conditions: {current_condition.get_text()}")
+
 except requests.Timeout:
     print("Request timed out")
 except requests.ConnectionError:
